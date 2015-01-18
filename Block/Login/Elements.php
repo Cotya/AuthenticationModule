9c6f5a0baed5f4a3c381a9807ae926c04813f011
Flyingmana <flyingmana@googlemail.com>
2015-01-18 04:34:14 +0100

    add login Link

---
+++ Block/Login/Elements.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+namespace Cotya\Authentication\Block\Login;
+
+
+use Magento\Framework\View\Element\AbstractBlock;
+
+class Elements extends AbstractBlock
+{
+
+    protected function _toHtml()
+    {
+        $html = '';
+        $githubLoginUrl = $this->getUrl('cotya_authentication/index/login');
+        $html .= '<a href="'.$githubLoginUrl.'">Login via Github</a>';
+        return $html;
+    }
+}
